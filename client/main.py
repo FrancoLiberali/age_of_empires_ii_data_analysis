a962fbe4bbf9eb3d25bb60679d5aa4dfa3c0b3fd
Franco Liberali <franco.liberali@acadeu.com>
2021-06-06 11:44:56 -0300

    pasar client de long matches a thread aparte

--- client/main.py
+++ client/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-import pika
 import csv
+import pika
+import threading
 
 from common.constants import STRING_ENCODING, \
     STRING_LINE_SEPARATOR, \
@@ -47,24 +48,20 @@ def send_sentinel(channel):
     )
 
 
-def get_print_matches_ids_function(connection, matches_ids):
-        # function currying in python
+def get_print_matches_ids_function(matches_ids):
+    # function currying in python
     def print_matches_ids(channel, method, properties, body):
         received_string = body.decode(STRING_ENCODING)
         if received_string == SENTINEL_MESSAGE:
-            print("Los IDs de matches que excedieron las dos horas de juego por pro players (average_rating > 2000) en los servers koreacentral, southeastasia y eastus son:")
-            print(', '.join(matches_ids))
+            print(f"Los IDs de matches que excedieron las dos horas de juego por pro players (average_rating > 2000) en los servers koreacentral, southeastasia y eastus son: {', '.join(matches_ids)}")
             channel.stop_consuming()
-            connection.close()
         else:
             for match_id in received_string.split(MATCHES_IDS_SEPARATOR):
                 matches_ids.append(match_id)
     return print_matches_ids
 
-def main():
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host=RABBITMQ_HOST))
-    channel = connection.channel()
+
+def request_long_matches(channel):
     channel.queue_declare(queue=CLIENT_TO_LONG_MATCHES_QUEUE_NAME)
     channel.queue_declare(queue=LONG_MATCHES_TO_CLIENT_QUEUE_NAME)
 
@@ -77,7 +74,7 @@ def main():
                 continue
             if (i % CHUCKSIZE_IN_LINES == 0 and i > 0):
                 send_chunk(channel, chunk)
-                del chunk[:] # delete from memory
+                del chunk[:]  # delete from memory
             chunk.append(get_line_string(line))
         send_chunk(channel, chunk)
         send_sentinel(channel)
@@ -85,11 +82,23 @@ def main():
     matches_ids = []
     channel.basic_consume(
         queue=LONG_MATCHES_TO_CLIENT_QUEUE_NAME,
-        on_message_callback=get_print_matches_ids_function(
-            connection, matches_ids),
+        on_message_callback=get_print_matches_ids_function(matches_ids),
         auto_ack=True
     )
     channel.start_consuming()
+
+def main():
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host=RABBITMQ_HOST))
+    channel = connection.channel()
+
+    long_matches_client = threading.Thread(
+        target=request_long_matches,
+        args=((channel),))
+    long_matches_client.start()
+
+    long_matches_client.join()
+    connection.close()
 
 if __name__ == '__main__':
     main()
