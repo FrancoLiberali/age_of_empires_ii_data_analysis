d2d4c66c10fb94a74c40ed0ecf5cd9552b3cca9d
Franco Liberali <franco.liberali@acadeu.com>
2021-06-08 14:19:28 -0300

    cantidad de reducers configurable

--- start_up.sh
+++ start_up.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+error () {
+	echo >&2 "$@"
+	exit 1
+}
+
+[ "$#" -eq 1 ] || error "Reducers amounter is required. Usage: ./start_up.sh <reducers_amount>"
+
 docker-compose -f docker-compose-rabbit.yaml up -d
 sleep 5
-export REDUCERS_AMOUNT=6
-# TODO que sea configurable al correr el comando
-docker-compose -f docker-compose-client-and-servers.yaml up -d --scale group_by_match_reducer_server=6
+export REDUCERS_AMOUNT=$1
+docker-compose -f docker-compose-client-and-servers.yaml up -d --scale group_by_match_reducer_server=$1
 docker-compose -f docker-compose-client-and-servers.yaml logs -f
