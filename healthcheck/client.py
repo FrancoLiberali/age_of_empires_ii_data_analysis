0bf3989b4f99263f7ec441ae2706fd1faec4aaef
lucas <lhemmingsen@fi.uba.ar>
2021-07-26 15:42:31 -0300

    Issue with ping service solved

--- healthcheck/client.py
+++ healthcheck/client.py
@@ -2,23 +2,23 @@ from logger.logger import Logger
 import socket
 
 PORT = 9999
-RECV=b'PING!'
-SEND=b'PONG!'
+SEND=b'PING!'
+RECV=b'PONG!'
 logger=Logger()
 
-def ping(host):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def ping(host):    
     try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((host, PORT))
-        sock.sendall(SEND)        
+        sock.sendall(SEND)    
+    except socket.error:
+        return False    
+    try:
+        data=b''
+        while len(data) < len(RECV):
+            data += sock.recv(len(RECV) - len(data))
     except socket.error:
         return False
-    data=b''
-    while len(data) < len(RECV):
-        try:
-            data += sock.recv(len(RECV) - len(data))
-        except socket.error:
-            return False
     if data != RECV:
         return False
     return True
--- healthcheck/server.py
+++ healthcheck/server.py
@@ -13,16 +13,17 @@ logger=Logger()
 class PingHandler(socketserver.BaseRequestHandler):
 
     def handle(self):
-        try:
-            self.data = self.request.recv(len(RECV))
-        except socket.errror as err:
-            logger.info("PingHandler: Socket error receiving ping request: {}".format(err.strerror))
+        data=b''
+        try:            
+            while len(data) < len(RECV):
+                data += self.request.recv(len(RECV) - len(data))
+        except socket.errror:
             return
-        if self.data == RECV:
+        if data == RECV:
             try:
                 self.request.sendall(SEND)
-            except socket.error as err:
-                logger.info("PingHandler: Socket error responding ping request: {}".format(err.strerror))
+            except socket.error:
+                pass
 
 
 def run():
