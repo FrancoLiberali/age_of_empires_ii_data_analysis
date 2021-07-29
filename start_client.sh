342e50471a295c33d2ca58e21b6b96d8d8497325
Franco Liberali <franco.liberali@acadeu.com>
2021-07-29 09:33:40 -0300

    poder correr varios clientes

--- start_client.sh
+++ start_client.sh
@@ -6,5 +6,5 @@ error () {
 
 [ "$#" -eq 2 ] || error "Datasets names required. Usage: ./start_client.sh matches_file_name players_file_name"
 
-MATCHES_FILE_NAME=$1 PLAYERS_FILE_NAME=$2 docker-compose -f docker-compose-client.yaml up -d
+MATCHES_FILE_NAME=$1 PLAYERS_FILE_NAME=$2 docker-compose -f docker-compose-client.yaml run client
 docker-compose -f docker-compose-client.yaml logs -f
